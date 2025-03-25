004be4f5fcdc31890cfffa3fceb4235ada5c22da
Álvaro <alvarocc948@gmail.com>
2025-03-25 10:37:45 +0100

    Movimiento horizontal del jugador

---
+++ spaceinvader2/player.gd
@@ -0,0 +1,23 @@
+extends Sprite2D
+
+@export var speed = 300
+var direction = Vector2.ZERO
+
+@onready var collision: CollisionShape2D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var movement = speed * delta * direction.x
+	# Entrada por teclado de movimientos horizontales
+	var input = Input.get_axis("move_left","move_right")
+	if input > 0:
+		direction = Vector2.RIGHT
+	elif input < 0:
+		direction = Vector2.LEFT
+	else:
+		direction = Vector2.ZERO
+	position.x += movement
